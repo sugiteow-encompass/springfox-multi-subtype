a5da00127fd8311f997ebc5461edfccabfa2a1f1
Sugiyarto Teow <sugiyartot@encompasscorporation.com>
2020-05-05 11:13:55 +1000

    fix type name.

--- src/main/java/blah/PolicyFormParameters.java
+++ src/main/java/blah/PolicyFormParameters.java
@@ -5,8 +5,8 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "type")
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = BothInputAndFormParameters.class, name = "DOG"),
-        @JsonSubTypes.Type(value = SomeFormParameters.class, name = "CAT")
+        @JsonSubTypes.Type(value = BothInputAndFormParameters.class, name = "BothInputAndFormParameters"),
+        @JsonSubTypes.Type(value = SomeFormParameters.class, name = "SomeFormParameters")
 })
 public interface PolicyFormParameters {
     String getType();
